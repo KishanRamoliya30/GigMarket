defa443b977186fc54a42a514db0d2c874038fad
Kishan Ramoliya <kishan.ramoliya@tatvasoft.com>
2025-07-02 11:59:59 +0530

    remove console

--- src/lib/getCurrentUser.ts
+++ src/lib/getCurrentUser.ts
@@ -11,7 +11,6 @@ export async function getUserFromSession(cookieStore: ReturnType<typeof cookies>
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: string };
-    console.log("####6", decoded)
     const user = await User.findById(decoded.userId).select('-password');
     return user || null;
   } catch (err) {
