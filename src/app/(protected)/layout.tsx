382077e36d6c84e93e38b3f9144cfdbf9c67a831
nisha.kadam <nisha.kadam@tatvasoft.com>
2025-07-17 17:22:20 +0530

    dashboard added

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Footer from "@/components/user/footer/Footer";
 import Header from "@/components/user/header/Header";
-const layout = ({
+import Sidebar from "@/components/dashboard/Sidebar";
+
+const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -9,10 +11,22 @@ const layout = ({
   return (
     <>
       <Header />
-      {children}
-      <Footer />
+
+      {/* Full page layout */}
+      <div className="pt-[64px] min-h-screen flex flex-col">
+        {/* Sidebar + Content */}
+        <div className="flex flex-1">
+          <div className="sticky top-[64px] h-[calc(100vh-64px)]">
+            <Sidebar />
+          </div>
+          <main className="flex-1 px-4 py-6">{children}</main>
+        </div>
+
+        {/* Footer always below */}
+        <Footer />
+      </div>
     </>
   );
 };
 
-export default layout;
+export default Layout;
--- src/components/landing/JoinFiveer.tsx
+++ src/components/landing/JoinFiveer.tsx
@@ -19,10 +19,10 @@ function JoinFiverr() {
         {/* Text Content */}
         <div className="absolute z-10 inset-0 flex flex-col justify-center items-start px-6 sm:px-10 md:px-16">
           <h4 className="text-white text-2xl sm:text-4xl md:text-5xl mb-6 font-semibold leading-snug max-w-xl">
-            Suddenly it&apos;s all so <i>doable.</i>
+            Freelance services at your <i>fingertips</i>
           </h4>
           <button
-            className="border text-sm sm:text-base font-medium px-5 py-2 border-[#1DBF73] bg-[#1DBF73] text-white rounded-md"
+            className="border cursor-pointer text-sm sm:text-base font-medium px-5 py-2 border-[#1DBF73] bg-[#1DBF73] text-white rounded-md"
             type="button"
             onClick={()=>router.push("/signup")}
           >
