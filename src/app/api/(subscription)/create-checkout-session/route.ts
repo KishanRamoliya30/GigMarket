c2b87a0b2af1c423d2418c1b12489c2d7519b3c5
Kishan Ramoliya <kishan.ramoliya@tatvasoft.com>
2025-07-17 18:29:07 +0530

    fix subscription issue in netlify

--- src/app/api/(subscription)/create-checkout-session/route.ts
+++ src/app/api/(subscription)/create-checkout-session/route.ts
@@ -133,9 +133,11 @@ export async function POST(request: NextRequest) {
       'Stripe session created',
       200
     );
-  } catch (error) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} catch (error:any) {
   console.error('Error creating checkout session:', error);
-  return successResponse(error, 'Failed to create checkout session', 500);
-  }
+  // return successResponse(error, 'Failed to create checkout session', 500);
+    return successResponse(error, error.message, 500);
+}
 
 }
