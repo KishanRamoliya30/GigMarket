c086c83ad06e2d15ec2d4619e171c9b4a2df5525
Kishan Ramoliya <kishan.ramoliya@tatvasoft.com>
2025-07-17 19:09:04 +0530

    remove console log

--- src/app/api/(subscription)/create-checkout-session/route.ts
+++ src/app/api/(subscription)/create-checkout-session/route.ts
@@ -27,115 +27,106 @@ const getSecret = () => new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function POST(request: NextRequest) {
   await dbConnect();
+
+  let body: RequestBody;
+
   try {
-    let body: RequestBody;
-
-    try {
-      body = await request.json();
-    } catch {
-      throw new ApiError('Invalid JSON body', 400);
-    }
-
-    const { payload } = await jwtVerify(
-      request.cookies.get("token")?.value ?? "",
-      getSecret()
-    );
-    console.log("#####80", payload)
-
-    const user = await User.findOne({ email: payload.email });
-    const email = user.email;
-
-    console.log("#####81", user)
-    if (!user) {
-      throw new ApiError('User not found', 404);
-    }
-
-    const { plan, successUrl, cancelUrl } = body;
-
-    if (!email || !plan || typeof plan.price !== 'number' || !successUrl || !cancelUrl) {
-      throw new ApiError('Invalid plan details or email', 400);
-    }
-
-    if (plan.price > 0 && !plan.priceId) {
-      throw new ApiError('Missing Stripe priceId for paid plan', 400);
-    }
-
-    if (plan.price <= 0) {
-      // Cancel existing Stripe subscription if any
-      if (user.subscription?.subscriptionId) {
-        const existingSub = await Subscription.findById(user.subscription.subscriptionId);
-
-        if (existingSub && existingSub.stripeSubscriptionId) {
-          await stripe.subscriptions.cancel(existingSub.stripeSubscriptionId);
-
-          await Subscription.findByIdAndUpdate(existingSub._id, {
-            status: 'cancelled',
-            endDate: new Date(),
-            canceledAt: new Date(),
-          });
-        }
-      }
-
-      // Update user with free plan
-      user.subscription = {
-        status: 'active',
-        subscriptionId: null,
-        planId: plan._id,
-        planName: plan.name,
-        planType: plan.type,
-        cancelAtPeriodEnd: false,
-      };
-      user.subscriptionCompleted = true;
-      await user.save();
-
-      return successResponse(
-        { message: 'Free subscription activated' },
-        'Subscription completed without payment',
-        200
-      );
-    }
-
-
-    let customerId = user.stripeCustomerId;
-
-    if (!customerId) {
-      const customer = await stripe.customers.create({ email });
-      customerId = customer.id;
-      user.stripeCustomerId = customerId;
-      await user.save();
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'subscription',
-      customer: customerId,
-      line_items: [
-        {
-          price: plan.priceId,
-          quantity: 1,
-        },
-      ],
-      subscription_data: {
-        metadata: {
-          userId: user._id.toString(),
-          planName: plan.name,
-          planData: JSON.stringify(plan),
-        },
-      },
-      success_url: successUrl,
-      cancel_url: cancelUrl,
-    });
-
-    return successResponse(
-      { id: session.id, url: session.url },
-      'Stripe session created',
-      200
-    );
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    console.error('Error creating checkout session:', error);
-    // return successResponse(error, 'Failed to create checkout session', 500);
-    return successResponse(error, error.message, 500);
+    body = await request.json();
+  } catch {
+    throw new ApiError('Invalid JSON body', 400);
   }
 
+  const { payload } = await jwtVerify(
+    request.cookies.get("token")?.value ?? "",
+    getSecret()
+  );
+
+  const user = await User.findOne({ email: payload.email });
+  const email = user.email;
+
+  if (!user) {
+    throw new ApiError('User not found', 404);
+  }
+
+  const { plan, successUrl, cancelUrl } = body;
+
+  if (!email || !plan || typeof plan.price !== 'number' || !successUrl || !cancelUrl) {
+    throw new ApiError('Invalid plan details or email', 400);
+  }
+
+  if (plan.price > 0 && !plan.priceId) {
+    throw new ApiError('Missing Stripe priceId for paid plan', 400);
+  }
+
+  if (plan.price <= 0) {
+    // Cancel existing Stripe subscription if any
+    if (user.subscription?.subscriptionId) {
+      const existingSub = await Subscription.findById(user.subscription.subscriptionId);
+
+      if (existingSub && existingSub.stripeSubscriptionId) {
+        await stripe.subscriptions.cancel(existingSub.stripeSubscriptionId);
+
+        await Subscription.findByIdAndUpdate(existingSub._id, {
+          status: 'cancelled',
+          endDate: new Date(),
+          canceledAt: new Date(),
+        });
+      }
+    }
+
+    // Update user with free plan
+    user.subscription = {
+      status: 'active',
+      subscriptionId: null,
+      planId: plan._id,
+      planName: plan.name,
+      planType: plan.type,
+      cancelAtPeriodEnd: false,
+    };
+    user.subscriptionCompleted = true;
+    await user.save();
+
+    return successResponse(
+      { message: 'Free subscription activated' },
+      'Subscription completed without payment',
+      200
+    );
+  }
+
+
+  let customerId = user.stripeCustomerId;
+
+  if (!customerId) {
+    const customer = await stripe.customers.create({ email });
+    customerId = customer.id;
+    user.stripeCustomerId = customerId;
+    await user.save();
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'subscription',
+    customer: customerId,
+    line_items: [
+      {
+        price: plan.priceId,
+        quantity: 1,
+      },
+    ],
+    subscription_data: {
+      metadata: {
+        userId: user._id.toString(),
+        planName: plan.name,
+        planData: JSON.stringify(plan),
+      },
+    },
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  });
+
+  return successResponse(
+    { id: session.id, url: session.url },
+    'Stripe session created',
+    200
+  );
 }
