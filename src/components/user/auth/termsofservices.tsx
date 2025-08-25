dd7e2bc9c6b0f809b8783360b6e1fdadc5fbb7d7
Sakib Memon <sakib.memon@tatvasoft.com>
2025-08-25 18:27:52 +0530

    Implement ssr on homepage providers section

--- src/components/user/auth/termsofservices.tsx
+++ src/components/user/auth/termsofservices.tsx
@@ -1,36 +1,18 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
 import { Box, Typography, Container, Divider } from "@mui/material";
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 import { apiRequest } from "@/app/lib/apiCall";
 
-const TermsConditions = () => {
-  const [contentHtml, setContentHtml] = useState("");
-  const [lastUpdated, setLastUpdated] = useState("");
-
-  useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        const data = await apiRequest("terms"); 
-        const sanitizedHtml = DOMPurify.sanitize(data.data.content);
-        setContentHtml(sanitizedHtml);
-
-        setLastUpdated(new Date(data.data.updatedAt).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        }));
-      } catch (error) {
-        console.error("Failed to load terms:", error);
-      }
-    };
-
-    fetchContent();
-  }, []);
-
-  return (
-    <Container maxWidth="md" sx={{ py: 6 }}>
+const TermsConditions = async () => {
+  try {
+    const data = await apiRequest("terms"); 
+    const sanitizedHtml = DOMPurify.sanitize(data.data.content);
+    const lastUpdated = new Date(data.data.updatedAt).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return (
+      <Container maxWidth="md" sx={{ py: 6 }}>
       <Typography variant="h4" fontWeight={700} gutterBottom>
         Terms of Service
       </Typography>
@@ -45,10 +27,26 @@ const TermsConditions = () => {
 
       <Box
         sx={{ typography: "body1" }}
-        dangerouslySetInnerHTML={{ __html: contentHtml }}
+        dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
       />
     </Container>
-  );
+    );
+  }catch (error) {
+        console.error("Failed to load terms:", error);
+
+    return (
+      <Container maxWidth="md" sx={{ py: 6 }}>
+        <Typography variant="h4" fontWeight={700} gutterBottom>
+          Terms of Service
+        </Typography>
+        <Typography variant="body2" color="error">
+          Failed to load terms. Please try again later. {error instanceof Error ? error.message : "Unknown error"}
+        </Typography>
+      </Container>
+    );
+  }
+
+
 };
 
 export default TermsConditions;
